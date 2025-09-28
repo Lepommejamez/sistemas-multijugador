af17d198224bf4bc274c5400b3d56585c4d617b4
Lepommejamez <98928651+Lepommejamez@users.noreply.github.com>
2025-09-28 18:43:17 +0200

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_connection = 'sqlite:..\private\users.db';
-$db = new PDO($db_connection);
-$sql = 'INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)';
-$query = $db->prepare($sql);
-$query->bindValue(':user_name', "admin2");
-$query->bindValue(':user_password', "password");
